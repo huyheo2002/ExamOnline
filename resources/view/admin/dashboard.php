b60e3b37c0e83f4aa7786027cc705efde9176bcd
huyheo2002 <huyheo2548@gmail.com>
2022-09-27 10:35:59 +0700

    Add: views dashboard

--- resources/view/admin/dashboard.php
+++ resources/view/admin/dashboard.php
@@ -10,6 +10,7 @@ require_once "./app/Route.php";
         include "./resources/view/admin/partitions/head.php";
     ?>
     <link rel="stylesheet" href="./resources/css/index.css">
+    <link rel="stylesheet" href="./resources/css/dashboard.css">
 </head>
 <body>
     <div id="main">
@@ -23,6 +24,58 @@ require_once "./app/Route.php";
                     include "./resources/view/admin/partitions/header.php";
                     ?>
                 <div class="content__main show">
+                    <h1 class="dashboard">
+                        Dashboard
+                    </h1>
+                    <div id="group">
+                        <ul class="group__list">
+                            <li class="group__list-item items__hover group__border"><a href="">
+                                <div class="group__list-bg" style="background: url(./resources/images/main/home/dashboard-bgitem1.jpg) center/cover no-repeat;" >
+                                    <div class="group__list-Overlay"></div>
+                                </div>                                
+                                <img src="./resources/images/main/home/dashboard-bgoverlay-exam-removebg-preview.png" alt="">
+                                <div class="group__items-textShow-wrap">
+                                    <p class="group__items-textShow">Quản lý đề thi</p>
+                                    <p class="group__items-subtextShow">58 đề thi</p>
+                                </div>
+                                
+                            </a></li>
+                            <li class="group__list-item items__hover group__border"><a href="">
+                                <div class="group__list-bg" style="background: url(./resources/images/main/home/dashboard-bgitem2.jpg) center/cover no-repeat;">
+                                    <div class="group__list-Overlay"></div>
+                                </div>                                
+                                <img src="./resources/images/main/home/dashboard-bgoverlay-question-removebg-preview.png" alt="">
+                                <div class="group__items-textShow-wrap">
+                                    <p class="group__items-textShow">Quản lý câu hỏi</p>
+                                    <p class="group__items-subtextShow">101 câu hỏi</p>
+                                </div>
+                            </a></li>                            
+                            <li class="group__list-item items__hover group__border"><a href="">
+                                <div class="group__list-bg" style="background: url(./resources/images/main/home/dashboard-bgitem-teacher.jpg) center/cover no-repeat;">
+                                    <div class="group__list-Overlay"></div>
+                                </div>                                
+                                <img src="./resources/images/main/home/point-1.png" alt="">
+                                <div class="group__items-textShow-wrap">
+                                    <p class="group__items-textShow">Quản lý giáo viên</p>
+                                    <p class="group__items-subtextShow">12 giáo viên</p>
+                                </div>
+                            </a></li>
+                            <li class="group__list-item items__hover group__border"><a href="">
+                                <div class="group__list-bg" style="background: url(./resources/images/main/home/dashboard-bgitem-student.jpg) center/cover no-repeat;">
+                                    <div class="group__list-Overlay"></div>
+                                </div>                                
+                                <img src="./resources/images/main/home/doExam-poster.png" alt="">
+                                <div class="group__items-textShow-wrap">
+                                    <p class="group__items-textShow">Quản lý học viên</p>
+                                    <p class="group__items-subtextShow">199 học viên</p>
+                                </div>
+                            </a></li>
+                            <li class="group__list-item reloadgroup">
+                                <div class="group__list-bg reloadgroup" style="background: url(./resources/images/main/home/dashboard-bgitem-reload.jpg) center no-repeat;"></div>
+                                <p>Vui Lòng đợi</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
