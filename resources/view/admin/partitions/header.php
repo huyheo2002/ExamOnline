98862bc212bdd6b5a5cf403d0a1a0840c4723451
longfn <longpn.golsoftvn@gmail.com>
2022-09-28 22:48:34 +0700

    Add header for admin

--- resources/view/admin/partitions/header.php
+++ resources/view/admin/partitions/header.php
@@ -1,5 +1,40 @@
 <?php
-
+require_once "./app/Route.php";
+require_once "./app/Auth.php";
+require_once "./app/models/Role.php";
 ?>
 
-<div class="content__header">this is header</div>
+<ul class="header nav nav-tabs">
+    <div class="nav-item"></div>
+    <?php
+    if (Auth::check()) {
+    ?>
+        <li class="nav-item dropdown">
+            <a class="header__avtUser nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <img class="rounded-circle" src="<?= Auth::user()->getAvatarLink() ?>" alt="">
+            </a>
+            <div class="dropdown-menu">
+                <?php
+                if (Auth::user()->role_id != Role::OF["student"]) {
+                ?>
+                    <a class="dropdown-item" href="<?= Route::path('admin') ?>">Vào giao diện quản lý</a>
+                <?php
+                }
+                ?>
+                <a class="dropdown-item" href="<?= Route::path('profile.show') ?>">Hồ sơ của bạn</a>
+                <a class="dropdown-item" href="<?= Route::path('test.history') ?>">Lịch sử làm bài của bạn</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="<?= Route::path('logout') ?>">Đăng xuất</a>
+            </div>
+        </li>
+    <?php
+    } else {
+    ?>
+        <li class="nav-item d-flex align-items-center">
+            <a href="<?= Route::path('login') ?>" class="nav-link">Đăng nhập</a>
+        </li>
+    <?php
+    }
+    ?>
+
+</ul>
